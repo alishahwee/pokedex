f0020da564cfdda7c6e5a8c273a7579166eea60c
alishahwee <alisha.hwee@outlook.com>
2020-09-14 23:32:13 -0500

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Alisha Hwee on 9/14/20.
+//  Copyright © 2020 Alisha Hwee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
